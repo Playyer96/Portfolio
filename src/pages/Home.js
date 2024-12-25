37043f64ed72ba98c1123871b9c3672b6442e6c5
Dani <dagugo.96@gmail.com>
2024-12-25 15:08:01 -0500

    [Update]: Home page updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,44 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import CardDisplay from "../components/CardDisplay";
 import ContactIcons from "../components/ContactIcons";
-import { SkillsList } from "../helpers/SkillsList";
+import {SkillsList} from "../helpers/SkillsList";
 import "../styles/Home.css";
 
 const Home = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <div className="home">
-      <div className="aboutme">
-        <h2>
-          Hi, I'm <span>Dani</span>
-        </h2>
-        <div className="prompt">
-          <p>Unreal And Unity developer with a passion to learn new stuff</p>
-          <ContactIcons />
-          <button onClick={() => navigate("/about")}>About Me!</button>
-        </div>
-      </div>
-      <div className="skills-section">
-        <h2>Skills</h2>
+    return (
+        <div className="home">
+            <div className="aboutme">
+                <h2>
+                    Hi, I'm <span>Dani</span>
+                </h2>
+                <div className="prompt">
+                    <p>Unreal And Unity developer with a passion to learn new stuff</p>
+                    <ContactIcons/>
+                    <button onClick={() => navigate("/about")}>About Me!</button>
+                </div>
+            </div>
+            <div className="skills-section">
+                <h1>Skills</h1>
 
-        <div className="skills">
-          <div className="skillList">
-            {SkillsList.map((skill, index) => {
-              return (
-                <CardDisplay
-                  key={index}
-                  image={skill.icon}
-                  tooltip={skill.text}
-                />
-              );
-            })}
-          </div>
+                <div className="skills">
+                    <div className="skillList">
+                        {SkillsList.map((skill, index) => {
+                            return (
+                                <CardDisplay
+                                    key={index}
+                                    image={skill.icon}
+                                    tooltip={skill.text}
+                                />
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default React.memo(Home);
