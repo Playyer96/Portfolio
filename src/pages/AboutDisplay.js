932597a91983735dc4871a91085cb474fe684c25
Dani <dagugo.96@gmail.com>
2025-01-06 13:33:05 -0500

    [Update]: about display description improved and also improve the style of the page

--- src/pages/AboutDisplay.js
+++ src/pages/AboutDisplay.js
@@ -1,35 +1,43 @@
 import React from "react";
 import profilePic from "../assets/profile-pic.png";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 import "../styles/AboutDisplay.css";
 
 function AboutDisplay() {
     return (
-        <div className="main">
-            <div className="about">
-                <img src={profilePic} alt="Profile"/>
-                <div className="about-text">
-                    <h1>
-                        Hi, I'm <span>Dani</span>
-                    </h1>
-                    <h5>
-                        Unity & Unreal <span>Game Developer</span>
-                    </h5>
-                    <p>
-                        With over six years of experience in game development, I specialize
-                        in creating immersive, high-performance experiences across platforms
-                        like PC, mobile, and XR. I bring expertise in Unity (C#) and Unreal Engine (C++/Blueprints), with a proven track
-                        record of leading multidisciplinary teams to deliver polished, performance-optimized projects.
-                    </p>
-                    <p>
-                        My passion lies in designing innovative gameplay mechanics, solving
-                        complex technical challenges, and pushing the boundaries of VR/AR
-                        experiences. Whether it's developing precise physics systems for a
-                        golf simulator or optimizing VR performance, I thrive on creating
-                        engaging, seamless user experiences.
-                    </p>
+        <TransitionGroup component={null}>
+            <CSSTransition timeout={500} classNames="page" unmountOnExit>
+                <div className="main">
+                    <div className="about">
+                        <img src={profilePic} alt="Profile"/>
+                        <div className="about-text">
+                            <h1>
+                                Hi, I'm <span>Dani</span>
+                            </h1>
+                            <h5>
+                                Unity & Unreal <span>Game Developer</span>
+                            </h5>
+                            <p>
+                                With over six years of experience in game development, I specialize
+                                in creating immersive, high-performance experiences across platforms
+                                like PC, mobile, and XR. I bring expertise in Unity (C#) and Unreal Engine
+                                (C++/Blueprints),
+                                with a proven track
+                                record of leading multidisciplinary teams to deliver polished, performance-optimized
+                                projects.
+                            </p>
+                            <p>
+                                My passion lies in designing innovative gameplay mechanics, solving
+                                complex technical challenges, and pushing the boundaries of Mobile | PC | VR | AR
+                                experiences. Whether it's developing precise physics systems for a
+                                golf simulator or optimizing VR performance, I thrive on creating
+                                engaging, seamless user experiences.
+                            </p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </CSSTransition>
+        </TransitionGroup>
     );
 }
 
