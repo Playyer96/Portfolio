13c11ab5ddac4e99c7b7316a611fa632daf00d87
Dani <dagugo.96@gmail.com>
2025-01-18 20:41:15 -0500

    [Update]: typo fix

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -61,66 +61,66 @@ function Modal({isOpen, closeModal, project}) {
     if (!isOpen && !isAnimating) return null;
 
     return (<div className={`modalOverlay ${isAnimating ? "fadeIn" : "fadeOut"}`} onClick={handleClose}>
-        <div className="modalContent" onClick={(e) => e.stopPropagation()}>
-            <h1>{project.name}</h1>
-            <div className="modalCarousel">
-                <div className="carouselNavigation prev">
-                    <button onClick={prevItem}>&lt;</button>
-                </div>
+            <div className="modalContent" onClick={(e) => e.stopPropagation()}>
+                <h1>{project.name}</h1>
+                <div className="modalCarousel">
+                    <div className="carouselNavigation prev">
+                        <button onClick={prevItem}>&lt;</button>
+                    </div>
 
-                <div className="carouselContent">
-                    <div className={`carouselItem ${isTransitioning ? "fadeOut" : "fadeIn"}`}>
-                        {currentIndex < project.images.length && (<img
-                            src={project.images[currentIndex].image}
-                            alt={project.name}
-                            className="carouselImage"
-                        />)}
-                        {currentIndex === project.images.length && project.videoUrl && (<div className="videoWrapper">
-                            <iframe
-                                src={project.videoUrl}
-                                title={project.name}
-                                allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                                className="carouselVideo"
-                            ></iframe>
-                        </div>)}
+                    <div className="carouselContent">
+                        <div className={`carouselItem ${isTransitioning ? "fadeOut" : "fadeIn"}`}>
+                            {currentIndex < project.images.length && (<img
+                                src={project.images[currentIndex].image}
+                                alt={project.name}
+                                className="carouselImage"
+                            />)}
+                            {currentIndex === project.images.length && project.videoUrl && (
+                                <div className="videoWrapper">
+                                    <iframe
+                                        src={project.videoUrl}
+                                        title={project.name}
+                                        allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                        allowFullScreen
+                                        className="carouselVideo"
+                                    ></iframe>
+                                </div>)}
+                        </div>
+                    </div>
+
+                    <div className="carouselNavigation next">
+                        <button onClick={nextItem}>&gt;</button>
                     </div>
                 </div>
 
-                <div className="carouselNavigation next">
-                    <button onClick={nextItem}>&gt;</button>
+                <div className="modalDescription">
+                    <h3>Description</h3>
+                    <p>{project.descriptions && project.descriptions.join(" ")}</p>
                 </div>
-            </div>
 
-            <div className="modalDescription">
-                <h3>Description</h3>
-                <p>{project.descriptions && project.descriptions.join(" ")}</p>
-            </div>
+                <div className="modalTechnologies">
+                    <h3>Technologies</h3>
+                    <div className="techIcons">
+                        {techIcons}
+                    </div>
+                </div>
 
-            <div className="modalTechnologies">
-                <h3>Technologies</h3>
-                <div className="techIcons">
-                    {techIcons}
+                <div className="modalResponsabilities">
+                    <h3>Responsibilities</h3>
+                    <ul>
+                        {project.responsabilities && project.responsabilities.map((task, index) => (
+                            <li key={index}>{task}</li>))}
+                    </ul>
+                    {project.link && (<div className="projectLink">
+                        <a href={project.link} target="_blank" rel="noopener noreferrer">
+                            View Project
+                        </a>
+                    </div>)}
                 </div>
             </div>
         </div>
-
-        <div className="modalResponsabilities">
-            <h3>Responsibilities</h3>
-            <ul>
-                {project.responsabilities && project.responsabilities.map((task, index) => (
-                    <li key={index}>{task}</li>))}
-            </ul>
-            {project.link && (<div className="projectLink">
-                <a href={project.link} target="_blank" rel="noopener noreferrer">
-                    View Project
-                </a>
-            </div>)}
-        </div>
-    </div>
-</div>
-)
-    ;
+    )
+        ;
 }
 
 export default Modal;
